9bd0026aa8fa29924f13b7b3ef27e880168bdd32
deanwen1 <deanwen1@yandex.ru>
2020-09-05 21:54:22 +0300

    add

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Bob", "Smith", (byte) 20);
+        userService.saveUser("Tom", "Jones", (byte) 25);
+        userService.saveUser("Jack", "Brown", (byte) 23);
+        userService.saveUser("Kurt", "Wilson", (byte) 27);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private final String URL = "jdbc:mysql://localhost:3306/jm?useSSL=false";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "yfcnhjqrb1";
+
+    private Connection connection;
+
+    public Util() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 }
